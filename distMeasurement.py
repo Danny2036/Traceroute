28acd940d3ef19deedd562303a5096d252594246
Danny2036 <dnm30@case.edu>
2015-12-01 23:34:54 -0500

    Attempting to make timeout work better

--- distMeasurement.py
+++ distMeasurement.py
@@ -1,52 +1,77 @@
 import socket
+#Referenced Ksplice blog tutorial on tracerouting
+#Referenced Silver moon raw socker programming in python
 
-
-def main(destinationame):
-    print('Looking for ' + destinationame)
-    destinationaddress = socket.gethostbyname(destinationame)
-    port = 33434
-    maxnumhops = 30
-    icmp = socket.getprotobyname('icmp')
-    udp = socket.getprotobyname('udp')
-    ttl = 1
-    timeoutlength = 1
-    while True:
-        currentaddress = None
-        currentname = None
-        recevingsocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-        recevingsocket.bind(("", port))
-        recevingsocket.settimeout(timeoutlength)
-        sendingsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
-        sendingsocket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-        sendingsocket.sendto("", (destinationame, port))
-        try:
-            _, currentaddress = recevingsocket.recvfrom(512)
-            currentaddress = currentaddress[0]
+def main(destinationame, retrynumber):
+    #If it does not does not recive a response after 3 attempts, stop trying
+    if retrynumber < 3:
+        print('Looking for ' + destinationame + '. Attempt' + str(retrynumber))
+        #Gets ip address based off of name
+        destinationaddress = socket.gethostbyname(destinationame)
+        port = 33434
+        maxnumhops = 128
+        icmp = socket.getprotobyname('icmp')
+        udp = socket.getprotobyname('udp')
+        ttl = 1
+        timeoutlength = 1
+        while True:
+            currentaddress = None
+            currentname = None
+            #Creates a socket to listen for responses
+            recevingsocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+            recevingsocket.bind(("", port))
+            recevingsocket.settimeout(timeoutlength)
+            #Creates a socket that will be sending out requests
+            sendingsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
+            sendingsocket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+            sendingsocket.sendto("", (destinationame, port))
             try:
-                currentname = socket.gethostbyaddr(currentaddress)[0]
+                _, currentaddress = recevingsocket.recvfrom(512)
+                currentaddress = currentaddress[0]
+                try:
+                    #Tries to get a name for ip address the is more than numbers
+                    currentname = socket.gethostbyaddr(currentaddress)[0]
+                except socket.error:
+                    #If no name to use, set the name to the ip address
+                    currentname = currentaddress
             except socket.error:
-                currentname = currentaddress
-        except socket.error:
-            pass
-        finally:
-            sendingsocket.close()
-            recevingsocket.close()
+                pass
+            finally:
+                sendingsocket.close()
+                recevingsocket.close()
 
-        if currentaddress is not None:
-            currenthost = "%s (%s)" % (currentname, currentaddress)
-        else:
-            currenthost = "*Timed out"
-        print("%d\t%s" % (ttl, currenthost))
+            timeoutcount = 0
+            if currentaddress is not None:
+                #If something is returned on the recieving socket
+                currenthost = "%s (%s)" % (currentname, currentaddress)
+                #If this prints it means something was recived and the timeouts were broken
+                timeoutcount = 0
+            else:
+                currenthost = "*Timed out"
+                timeoutcount += 1
+            print("%d\t%s" % (ttl, currenthost))
 
-        ttl += 1
-        if currentaddress == destinationaddress or ttl > maxnumhops:
-            break
+            if timeoutcount < 5:
+                #If not enough response retry from the beginning
+                main(destinationame, retrynumber +1)
+                break
+            else:
+                print('Could not reach destination')
+                break
+
+            ttl += 1
+            if currentaddress == destinationaddress or ttl > maxnumhops:
+                #If the destination is found or the number of hops has been exceeded then end
+                break
+    else:
+        return
 
 
 if __name__ == "__main__":
     targetwebsites = open('targets.txt')
     for line in targetwebsites:
-        main('line')
+        #Remove carraige return
+        main(line.strip(), 0)
         print('Done with ' + line)
     print('Done with everything')
     #main('google.com')
